b57679bfe2a5f0e6f96edb7679f5888b18a41599
Aaron Berns <anberns@gmail.com>
2019-01-10 13:57:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,12 @@ class Shoe
     @brand = brand
   end
   
+  def brand
+    @brand
+  end
+  
   def cobble
-    puts "The shoe has been repaired."
+    puts "Your shoe is as good as new!"
     @condition = "new"
   end
   
