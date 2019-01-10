1c86228d80667b89f3b1bbeedfd03df41b453ec8
Aaron Berns <anberns@gmail.com>
2019-01-10 13:52:52 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,37 +1,16 @@
 class Book 
   
+  #automatic get and set for attributes
+  attr_accessor :author, :page_count, :genre
+  
   def initialize(title)
     @title = title
   end
   
+  
   # getter
   def title
     @title 
-  end
-  
-  # setter
-  def author=(author)
-    @author = author
-    
-  # getter
-  def author
-    @author
-  end
-  
-  def page_count=(num)
-    @page_count = num
-  end
-  
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
   end
   
   def turn_page
