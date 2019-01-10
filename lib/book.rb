c6293a6a20306cd09fe32877b3a29a47b416f3f0
Aaron Berns <anberns@gmail.com>
2019-01-10 13:50:09 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,30 @@
-
+class Book 
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  # getter
+  def title
+    @title 
+  end
+  
+  # setter
+  def author=(author)
+    @author = author
+    
+  # getter
+  def author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+end
 
